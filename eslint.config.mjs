900d383e3792b30f20a4bfa4bae6db5c4dc1d601
andrespastrana-it <andres@billimd.com>
2025-02-16 13:08:05 -0300

    refactor: update ESLint configuration to disable TypeScript unsafe function type rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,7 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
+    "@typescript-eslint/no-unsafe-function-type": "off",
   },
 });
 

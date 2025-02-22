8c7672452074a37d72fe00a8cd86a84fb25ed272
andrespastrana-it <andres@billimd.com>
2025-02-22 13:10:39 -0300

    fix: improve database connection options and clean up story retrieval logic

--- app/api/stories/route.ts
+++ app/api/stories/route.ts
@@ -1,22 +1,12 @@
-export const dynamic = 'force-static'
 
 
 import { Story } from "@/lib/models";
 import {  NextResponse } from "next/server";
-// TODO
-// Only allow request from my app domain
-// Secret key in request
-
 
 
 // Get all stories (SORTED BY DATE)
 export async function GET() {
-
-    const stories = await Story.find()
-    
-    // Check the secret key in the Req Auth
-    // Query the database
-    // Return the data
+    const stories = await Story.find({})
      return NextResponse.json({ success:true,error: false,data: stories})
 }
 
